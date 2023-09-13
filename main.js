28c7e5f2180ea42ac3e056171ee43618876587e9
JoefryT <109338652+JoefryT@users.noreply.github.com>
2023-09-13 18:55:49 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX = 0;
+noseY=0; 
+function preload(){
+    moustache = loadImage("https://i.postimg.cc/3N5WTvPT/LTdj-Ljr-Xc.png");
+}
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+
+}
+function draw(){
+  image(video, 0 , 0, 300, 300);
+  image(moustache, noseX, noseY, 100, 25)
+}
+function take_snapshot(){
+    save('myFilterImage.png'); 
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x-50;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = "+results[0].pose.nose.x);
+        console.log("nose y = "+results[0].pose.nose.y);
+    }
+}
